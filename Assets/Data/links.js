6eb0d2d0f812636ca394f6a095eebb3bda971044
Lucas Hansen <lucashansen0521@gmail.com>
2024-09-16 16:40:38 -0400

    Updated Data for testing

--- Assets/Data/links.js
+++ Assets/Data/links.js
@@ -15,4 +15,100 @@ const data = [
             "source": "twitter"
         }
     },
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "pfp": "https://pbs.twimg.com/profile_images/1819821904653365248/LCaWV3wa_400x400.jpg",
+        "details": {
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    },
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "pfp": "https://pbs.twimg.com/profile_images/1819821904653365248/LCaWV3wa_400x400.jpg",
+        "details": {
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    },
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "pfp": "https://pbs.twimg.com/profile_images/1819821904653365248/LCaWV3wa_400x400.jpg",
+        "details": {
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    },
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "pfp": "https://pbs.twimg.com/profile_images/1819821904653365248/LCaWV3wa_400x400.jpg",
+        "details": {
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    },
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "pfp": "https://pbs.twimg.com/profile_images/1819821904653365248/LCaWV3wa_400x400.jpg",
+        "details": {
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    },
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "pfp": "https://pbs.twimg.com/profile_images/1819821904653365248/LCaWV3wa_400x400.jpg",
+        "details": {
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    }
 ];
